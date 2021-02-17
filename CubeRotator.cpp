1825b33b47b75c8fb550fe77bfd8a99b1b5a7a58
ardayildirim <arda.yildirim158@gmail.com>
2021-02-17 11:07:11 +0300

    Improved CubeRotator class

--- CubeRotator.cpp
+++ CubeRotator.cpp
@@ -14,19 +14,62 @@ CubeRotator::CubeRotator(int screen_width,int screen_height)
 
     pointDensity = 6;
     a = 3;
-    points = new double**[6];
+    points = new vec3**[6];
+
     for(int side = 0; side < 6; side++)
     {
-        points[side] = new double*[screen_width];
-        for(int x = 0; x < screen_width; x++)
+        points[side] = new vec3*[pointDensity];
+        for(int x = 0; x < pointDensity; x++)
         {
-            points[side][x] = new double[screen_height];
+            points[side][x] = new vec3[pointDensity];
         }
     }
     
-
     double interval = a / (pointDensity - 1);
-    
+
+
+    double x,y,z;
+
+    x = -a/2;
+    for(int i = 0; i < pointDensity; i++)
+    {
+        y = -a/2;
+        for(int j = 0; j < pointDensity; j++)
+        {
+            points[0][i][j] = vec3(x,y,a/2);
+            points[1][i][j] = vec3(x,y,-a/2);
+            y += interval;
+        }
+        x += interval;
+        
+    }
+
+    z = a/2;
+    for(int i = 0; i < pointDensity; i++)
+    {
+        y = -a/2;
+        for(int j = 0; j < pointDensity; j++)
+        {
+            points[2][i][j] = vec3(a/2,y,z);
+            points[3][i][j] = vec3(-x/2,y,z);
+
+            y += interval;
+        }
+        z -= interval;
+    }
+
+	x = -a/2;
+	for(int i = 0; i < pointDensity; i++)
+	{
+		z = a/2;
+		for(int j = 0; j < pointDensity; j++)
+		{
+			points[4][i][j] = vec3(x,a/2,z);
+			points[5][i][j] = vec3(x,-a/2,z);
+		}
+		x += interval;
+		z -= interval;
+	}
 
 
 }
@@ -40,7 +83,7 @@ void CubeRotator::destructor()
 {
     for(int side = 0; side < 6; side++)
     {
-        for(int x = 0; x < screen_width; x++)
+        for(int x = 0; x < pointDensity; x++)
         {
             delete [] points[side][x];
         }
@@ -67,3 +110,37 @@ void CubeRotator::start()
     
 }
 
+CubeRotator::vec3::vec3()
+{
+    x = y = z = 0;
+}
+
+CubeRotator::vec3::vec3(double _x,double _y,double _z)
+{
+    x = _x; y = _y; z = _z;
+}
+
+CubeRotator::vec3 CubeRotator::vec3::operator*(const double& rhs)
+{
+    return vec3(x*rhs,y*rhs,z*rhs);
+}
+
+CubeRotator::vec3 CubeRotator::vec3::operator+(const vec3& rhs)
+{
+    return vec3(x+rhs.x, y+rhs.y, z+rhs.z);
+}
+
+double CubeRotator::vec3::operator[](int ind)
+{
+    switch(ind)
+    {
+        case 0:
+            return x;
+        case 1:
+            return y;
+        case 2:
+            return z;
+        default:
+            break;
+    }
+}
--- CubeRotator.h
+++ CubeRotator.h
@@ -6,24 +6,23 @@
 class CubeRotator
 {
 private:
-	double a; // a = length of any side of the cube.
-	double pointDensity; // there are pointDensity number of points on one side of the cube, it is like resolution or sensitivity
-	int screen_width,screen_height;
-	double *** points; // <- 3-star-programmer (https://wiki.c2.com/?ThreeStarProgrammer) who uses std::vector in c++ anyway.
-	void render_frame(double A,double B); //
-
 	struct vec3
 	{
 		double x,y,z;
 		vec3();
 		vec3(double,double,double);
-		vec3 operator*(const double&);
+		vec3 operator*(const double& rhs);
 		vec3 operator+(const vec3&);
 		double operator[](int ind);
 	};
 
+	double a; // a = length of any side of the cube.
+	double pointDensity; // there are pointDensity number of points on one side of the cube, it is like resolution or sensitivity
+	int screen_width,screen_height;
+	vec3 *** points; // <- 3-star-programmer (https://wiki.c2.com/?ThreeStarProgrammer) who uses std::vector in c++ anyway.
+	void render_frame(double A,double B); //
+
 public:
-    
 	CubeRotator();
 	CubeRotator(int scrWidth,int scrHeight); 
 	~CubeRotator();
@@ -31,8 +30,7 @@ public:
 	void destructor();
 	void start();
 	
-
-
+	void pointsPrint();
 };
 
 
